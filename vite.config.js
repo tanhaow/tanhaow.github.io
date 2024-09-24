c60ec9b12da9dc026c8fd39682a8875679703fdc
macTan <tanhaosmail@gmail.com>
2024-09-24 16:33:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
-  base: '/rachel-tanhao.github.io/',
+  // base: '/rachel-tanhao.github.io/',
 })
